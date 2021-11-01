f52a3ace69b489135c5d74fe50034d57bc0b0ff1
Gamaded <66965340+Gamaded@users.noreply.github.com>
2021-11-01 20:35:51 +0500

    renamed store to .ts extension

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore } from "redux";
 import thunkMiddleware from "redux-thunk";
-import appData from "./reducers/reducers.ts";
+import appData from "./reducers/reducers";
 
 const store = createStore(appData, applyMiddleware(thunkMiddleware));
 
